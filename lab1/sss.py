41fbc6dd17bad6ed4923c4e8988d74605a67a0bd
nurislam0070 <nurislambelike@gmail.com>
2026-02-03 22:52:49 +0500

    Update lab1

--- lab1/sss.py
+++ lab1/sss.py
@@ -1,1 +1,22 @@
-print(4+5)
+# n = int(input())
+# x = list(map(int, input().split()))
+# d = x[0]
+# e =0
+# for i in range(n):
+#     if x[i] > d :
+#         d = x[i]
+#         e = i
+# print(e+1)
+
+
+
+
+n = int(input())
+x = list(map(int, input().split()))
+d = x[0]
+e =0
+for i in range(n):
+    if x[i] > d :
+        d = x[i]
+        e = i
+print(e+1)
