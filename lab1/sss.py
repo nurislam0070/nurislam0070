469c0379e0ff279954fe00649b8f11ddad43899e
nurislam0070 <nurislambelike@gmail.com>
2026-02-03 23:10:25 +0500

    Update lab1

--- lab1/sss.py
+++ lab1/sss.py
@@ -9,14 +9,15 @@
 # print(e+1)
 
 
-
-
 n = int(input())
-x = list(map(int, input().split()))
-d = x[0]
-e =0
-for i in range(n):
-    if x[i] > d :
-        d = x[i]
-        e = i
-print(e+1)
+
+if n <= 1:
+    print("No")
+else:
+    s = True
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            s = False
+            break
+    print("Yes" if s else "No")
+
