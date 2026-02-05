66b7e45b774cb3e70ae8cfbe0468d092a559a816
nurislam0070 <nurislambelike@gmail.com>
2026-02-05 22:35:41 +0500

    Complete lab1

---
+++ lab1/basic_functions.py
@@ -0,0 +1,24 @@
+def my_function():
+  print("Hello from a function")
+my_function()
+def my_function():
+    pass
+
+
+def get_greeting():
+  return "Hello from a function"
+message = get_greeting()
+print(message)
+
+
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
+
+
+
+def get_greeting():
+  return "Hello from a function"
+print(get_greeting())
