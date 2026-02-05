82735cefe0f5be6381d6c0829258106efb1bccd2
nurislam0070 <nurislambelike@gmail.com>
2026-02-05 21:57:45 +0500

    Update lab1

--- lab1/sss.py
+++ lab1/sss.py
@@ -1,23 +1,10 @@
-# n = int(input())
-# x = list(map(int, input().split()))
-# d = x[0]
-# e =0
-# for i in range(n):
-#     if x[i] > d :
-#         d = x[i]
-#         e = i
-# print(e+1)
-
-
-n = int(input())
-
-if n <= 1:
-    print("No")
+n = input()
+fact = True
+for i in n:
+    if int(i) % 2 !=0:
+        fact = False
+        break
+if fact:
+    print("Valid")
 else:
-    s = True
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            s = False
-            break
-    print("Yes" if s else "No")
-
+    print("Not valid")
