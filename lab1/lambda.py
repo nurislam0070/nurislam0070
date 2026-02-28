60650731a52b2ece2dd18dba64e23f1b2aa2d89d
nurislam0070 <nurislambelike@gmail.com>
2026-02-28 21:58:56 +0500

    Complete lab1:basic_funtions, lambda

--- lab1/lambda.py
+++ lab1/lambda.py
@@ -1,10 +1,46 @@
-n = input()
-fact = True
-for i in n:
-    if int(i) % 2 !=0:
-        fact = False
+from datetime import datetime, timedelta
+import re
+import math
+
+def parse(line):
+    # Пример строки: "2000-02-29 UTC+06:00"
+    m = re.match(r'(\d{4})-(\d{2})-(\d{2}) UTC([+-])(\d{2}):(\d{2})', line)
+    y, mo, d, sign, hh, mm = m.groups()
+    dt = datetime(int(y), int(mo), int(d))
+    offset = timedelta(hours=int(hh), minutes=int(mm))
+    
+    if sign == '+':
+        utc = dt - offset
+        tz = offset
+    else:
+        utc = dt + offset
+        tz = -offset
+    return dt, utc, tz
+
+# Ввод даты рождения и текущей даты
+birth_local, _, birth_tz = parse(input().strip())
+_, current_utc, _ = parse(input().strip())
+
+bmo, bd = birth_local.month, birth_local.day
+
+def is_leap(y):
+    return y % 4 == 0 and (y % 100 != 0 or y % 400 == 0)
+
+def make_bday(y):
+    d = bd
+    # если 29 февраля в невисокосный год → переносим на 28 февраля
+    if bmo == 2 and bd == 29 and not is_leap(y):
+        d = 28
+    local = datetime(y, bmo, d)
+    return local - birth_tz  # переводим в UTC
+
+cy = current_utc.year
+
+# ищем первый день рождения UTC >= current_utc
+for y in [cy, cy+1]:
+    b_utc = make_bday(y)
+    if b_utc >= current_utc:
+        diff_seconds = (b_utc - current_utc).total_seconds()
+        diff_days = math.ceil(diff_seconds / 86400)  # округляем до целых дней
+        print(diff_days)
         break
-if fact:
-    print("Valid")
-else:
-    print("Not valid")
